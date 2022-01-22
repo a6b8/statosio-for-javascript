518bab1c72782396021e32abe46d45b2936176da
a6b8 <andreasbanholzer@me.com>
2022-01-22 19:06:11 +0100

    fix headlines

--- test/circle-ci.js
+++ test/circle-ci.js
@@ -1,38 +1,47 @@
-function addStr( str, index, insert ){
-  return str.substring( 0, index ) + insert + str.substring( index, str.length );
-}
-
-
 const fs = require( 'fs' )
 const puppeteer = require( 'puppeteer' )
 
 try {
   async function test() {
-    const statosio = fs.readFileSync( './dist/statosio.js' ,{ encoding: 'utf8' } )
-    const template = fs.readFileSync( './test/files/template.html' ,{ encoding: 'utf8' } )
-    
+    const statosio = fs.readFileSync( 
+        './dist/statosio.js',
+        { encoding: 'utf8' } 
+    )
+    const template = fs.readFileSync( 
+        './test/files/template.html',
+        { encoding: 'utf8' } 
+    )
     const htmls = template
         .split( '//insert statosio' )
     htmls.splice( 1, 0, statosio )
     html = htmls.join( '' )
-  
-    const browser = await puppeteer.launch()
+
+    const browser = await puppeteer.launch( { headless: false } )
     const page = await browser.newPage()
-    await page.setContent( 
-        html, 
-        { waitUntil: [ 'load','networkidle0' ] } 
+
+    const buff = new Buffer( html )
+    const base64data = buff.toString( 'base64' )
+    const data_str = `data:text/html;base64,${base64data}`
+
+    await page.goto(
+      data_str, 
+      { waitUntil: ["load","networkidle0"] }
     )
-  
+
     const test = await page.evaluate(() => {
         return document
             .getElementById( 'd3_statosio' )
             .getElementsByTagName( 'svg' )[ 0 ]
             .outerHTML
-    });
-  
-    const original = fs.readFileSync( './test/files/compare.html' ,{ encoding: 'utf8' } )
+    } )
+
+    const original = fs.readFileSync( 
+        './test/files/compare.html',
+        { encoding: 'utf8' } 
+    )
+    
     await browser.close()
-  
+
     if( original === test ) {
         console.log( 'Test passed.' )
         process.exit( 0 )
